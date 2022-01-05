e6f5783d2debc6f1e13ebcab1497682ae6359728
Rohan Sikand <rsikand@stanford.edu>
2022-01-05 03:56:11 -0500

    Upload to Github

---
+++ README.md
@@ -0,0 +1,79 @@
+# 🔖 Lemnos: To-do List Manager
+
+This is a simple program that allows one to manage a to-do list via the command-line. 
+
+## Example 
+
+```
+$ python3 todo.py add make pasta
+$ python3 todo.py show
+    1. make pasta
+    Number of items: 1
+$ python3 todo.py complete 1
+```
+
+## Set up
+
+You need to have Python installed. 
+
+1. Clone/download this repository. 
+2. `cd` into the directory when using the program. 
+
+You are good to go! You can optionally add a shebang of your Python install location to the top of `todo.py`. You can find this by typing `which python` in the command-line. Prepend that string with `#!` and add it to the top of `todo.py`. Example string: `#!/usr/bin/python3`. Then, you can simply type `./todo` to execute the program instead. To follow the reference then, just replace `python3 todo.py` with `./todo`. 
+
+## Reference
+
+- **Add item to list**: 
+  
+  ```
+  $ python3 todo.py add item
+  ```
+
+- **Show to-do list**:
+  
+  ```
+  $ python3 todo.py show
+  ```
+  
+  
+  - or simply: 
+  
+    ```
+    $ python3 todo.py
+    ```
+
+- **Complete item at index i**
+  
+  ```
+  $ python3 todo.py complete i
+  ```
+  
+
+- **Erase entire list**:
+  
+  ```
+  $ python3 todo.py erase
+  ```
+
+- **Get length of list**:
+  
+  ```
+  $ python3 todo.py len
+  ```
+  
+### Abbreviations 
+For efficiency purposes, you can simply specify the first letter of each functionality instead of the entire word. Example of adding `item` to the list. 
+```
+$ python3 todo.py a item
+```
+
+The program works by adding and removing content from `list.txt` so you can alternatively manage the list via that file as well. 
+
+## Note
+
+I recognize that there are probably many packages/programs out there that already have this functionality, but I wanted to build one from source so that I can feasibly add customizations to my personal setup as needed. Another motivation for creating this program was that the other options available are much more feature-heavy than I needed (a minimalist user experience is the main design principle here). 
+
+
+## Name 
+
+[Hephaestus](https://en.wikipedia.org/wiki/Hephaestus) is the Greek god of blacksmiths, metalworking, carpenters, craftsmen, among others. He resides in [Lemnos](https://en.wikipedia.org/wiki/Lemnos), which, in real life, is a Greek island in the Aegean Sea. 
---
+++ todo.py
@@ -0,0 +1,128 @@
+"""
+File: todo.py
+------------------
+Simple command-line to-do list manager. See README.md for getting started 
+and how to use the program. 
+"""
+
+import sys 
+
+LIST_FILE_PATH = "list.txt"
+
+
+def length(file_obj):
+	"""
+	Function that takes in the file object 
+	(pointing to list.txt) and returns the number of
+	items in the list. 
+	"""
+	count = 0
+
+	for line in file_obj:
+		count += 1 
+
+	return count
+
+
+def add(item_string):
+	"""
+	Function that appends the item based on what the 
+	user specified as a command line argument to list.txt. 
+	"""
+	curr_num_items = length(open(LIST_FILE_PATH))
+	curr_index = curr_num_items + 1
+	og_string = item_string  # for printing purposes 
+	item_string = str(curr_index) + ". " + item_string + '\n'
+	file = open(LIST_FILE_PATH, "a")
+	file.write(item_string)
+	file.close()
+
+	print('Added "' + og_string + '" to the to-do list.') 
+
+
+def show():
+	"""
+	Function that prints out the list. It also displays 
+	the number of items in the list. 
+	"""
+
+	file = open(LIST_FILE_PATH)
+
+	for line in file:
+		print(line, end='')  # use end to avoid printing \n 
+
+	print("Number of items:", length(open(LIST_FILE_PATH)))
+
+
+def complete(idx):
+	"""
+	Function that completes (deletes) the line (item) at the 
+	specified index from the list. 
+	"""
+
+	# store list of lines to use later (since we will overwrite these lines) 
+	temp_file_obj = open(LIST_FILE_PATH, "r") 
+	line_list = temp_file_obj.readlines()
+	temp_file_obj.close()
+	
+
+	updated_list = open(LIST_FILE_PATH, "w") 
+	curr_idx = 1
+	line_count = 0  # num lines appended to new list 
+	for line in line_list:
+		if curr_idx < idx:
+			updated_list.write(line)
+			line_count += 1
+		if curr_idx > idx:
+			line_count += 1 
+			line = str(line_count) + line[1:]
+			updated_list.write(line)
+
+		curr_idx += 1 
+
+
+def erase():
+	"""
+	Function that erases the contents of the list. 
+	"""
+
+	file = open(LIST_FILE_PATH, "w")
+	file.close() 
+
+
+def main():
+	user_arguments = sys.argv[1:]
+	arg_count = len(user_arguments)  # num user arguments 
+
+	# exit program if no user arguments were specified 
+	if arg_count == 0:
+		show()  # show the list 
+		sys.exit() 
+
+	# add item to list if 'add' argument is specified 
+	if (user_arguments[0] == "add" or user_arguments[0] == "a") and arg_count > 1:
+		add(' '.join(user_arguments[1:]))
+
+	# print out list if 'show' argument is specified
+	if user_arguments[0] == "show" or user_arguments[0] == "s":
+		show()
+
+	# print out list if 'show' argument is specified
+	if user_arguments[0] == "len" or user_arguments[0] == "l":
+		print("Number of items:", length(open(LIST_FILE_PATH)))
+
+	# complete (delete) specific item from list 
+	if (user_arguments[0] == "complete" or user_arguments[0] == "c") and arg_count == 2:
+		specified_idx = int(user_arguments[1]) 
+		if specified_idx > length(open(LIST_FILE_PATH)) or specified_idx <= 0:
+			print("Item index out of bounds")
+			sys.exit() 
+		complete(specified_idx)
+
+	# erase items from list 
+	if user_arguments[0] == "erase" or user_arguments[0] == "e":
+		erase()
+
+
+if __name__ == '__main__':
+    main()
